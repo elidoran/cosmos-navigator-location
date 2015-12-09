4e4ebb2414c4181d415a18faa67cf5daac30e866
Eli Doran <eli@elidoran.com>
2015-12-09 18:50:31 -0500

    adapt to use Running conditionally as a weak import

--- client/export.js
+++ client/export.js
@@ -2,16 +2,23 @@
 // create object we're exporting. We'll actually define it in a CoffeeScript file
 Nav = {}
 
-// function to call Nav.start() and Nav.stop()
-function runNav(running) {
-  if(running)
-    if (Nav.running !== true) Nav.start();
-  else
-    Nav.stop()
+// use Running instead of Meteor.startup() if available
+if (Package['cosmos:running'] && Package['cosmos:running'].Running) {
+  // function to call Nav.start() and Nav.stop()
+  function runNav(running) {
+    if (running)
+      if (Nav.running !== true) Nav.start();
+    else
+      Nav.stop()
+  }
+
+  // set its id. no before/after because others will order in relation to this one
+  runNav.options = {id:'CosmosRunNav'};
+
+  Package['cosmos:running'].Running.onChange(runNav);
 }
 
-// set its id. no before/after because others will order in relation to this one
-runNav.options = {id:'CosmosRunNav'};
-
-// use Running instead of Meteor.startup()
-Running.onChange(runNav);
+else // use startup()
+  Meteor.startup(function startupNav() {
+    Nav.start();
+  });
